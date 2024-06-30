3794ce25c2024e14f27431f64c096ec3dd23d826
Samuel Barone <samuel.barone@gmail.com>
2024-06-30 16:26:33 -0500

    ok1

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Neat Playground from Skeeter");
+  res.send("Neatest Playground from Skeeter");
 });
 
 app.listen(port, () => {
