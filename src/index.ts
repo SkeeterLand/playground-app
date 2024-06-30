f28566ee76e5d2f2795c7984b41157d205de0cba
Samuel Barone <samuel.barone@gmail.com>
2024-06-30 16:16:15 -0500

    it's neat

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Playground from Skeeter");
+  res.send("Neat Playground from Skeeter");
 });
 
 app.listen(port, () => {
