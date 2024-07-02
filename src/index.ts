259ab3a307b84e134eb0d15d4db464e4ed4224c1
Samuel Barone <samuel.barone@gmail.com>
2024-07-01 20:23:46 -0500

    Neat4

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Because of how it is");
+  res.send("It's Neat Because of how it is");
 });
 
 app.listen(port, () => {
